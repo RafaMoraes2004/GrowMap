5cc872de66ccd11b310d1774113bb68b7e17c855
RafaMoraes2004 <rafamoraescosta01@gmail.com>
2025-10-27 12:58:09 -0300

    Primeira Versao GrowMap

---
+++ src/app/cadastro/cadastro.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [RouterLink],
+  standalone: true,
+  templateUrl: './cadastro.html',
+  styleUrl: './cadastro.css'
+})
+export class Cadastro {
+
+}
